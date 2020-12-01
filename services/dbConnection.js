095f5f54c59e82e25809bf02984538705a2d77fa
AnkleBreaker <MagallonJanjan@gmail.com>
2020-12-01 21:32:56 +0800

    Finish connecting to mongodb atlas from local

--- services/dbConnection.js
+++ services/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = () => {
     mongoose
-      .connect("mongodb://127.0.0.1/management", {
+      .connect("mongodb://josephJohn:josephJohn@cluster0-shard-00-00.shguv.mongodb.net:27017,cluster0-shard-00-01.shguv.mongodb.net:27017,cluster0-shard-00-02.shguv.mongodb.net:27017/jobChase?ssl=true&replicaSet=atlas-odtvhg-shard-0&authSource=admin&retryWrites=true&w=majority", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
